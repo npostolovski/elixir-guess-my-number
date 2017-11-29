1a43d9b8167821f994aeb79ec0f3436c36b6b776
Tash Postolovski <npostolovski@gmail.com>
2017-11-29 21:26:43 +1100

    Ran mix new to generate base application structure.

---
+++ lib/guess_my_number.ex
@@ -0,0 +1,22 @@
+defmodule GuessMyNumber do
+  @moduledoc """
+  Documentation for GuessMyNumber.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GuessMyNumber.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def main(args) do
+    IO.puts "Hello world"
+  end
+end
